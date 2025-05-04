8ad493a35042bced51e0d8cc86a2c900c6b51d26
aaron031291 <shipton1234@gmail.com>
2025-05-04 17:41:11 +0100

    Update __intit__.py

--- grace_core_systems/central_intelligance/__intit__.py
+++ grace_core_systems/central_intelligance/__intit__.py
@@ -1,1 +1,7 @@
 from .auto_updater import LearningAutoUpdater
+# Main.py
+from grace_core_systems.central_intelligence import core_launcher
+
+# Temporary placeholder to run until Grace's runtime is wired
+if __name__ == "__main__":
+    core_launcher.run()
